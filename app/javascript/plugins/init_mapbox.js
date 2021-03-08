42ee60ec11cc52a583d5a0002d4b553c50a81cde
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-03-08 21:34:00 +1100

    completed map feature for now

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -2,31 +2,40 @@ import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
 const initMapbox = () => {
+  // Select map via id = 'map'
   const mapElement = document.getElementById("map");
 
-  // Zooms in when it goes to location
-  const fitMapToMarkers = (map, markers) => {
-    const bounds = new mapboxgl.LngLatBounds();
-    markers.forEach((marker) => bounds.extend([marker.lng, marker.lat]));
-    map.fitBounds(bounds, { padding: 70, maxZoom: 13, duration: 0 });
-  };
-
-  // creates the map
+  // Creates the map
   if (mapElement) {
-    // only build a map if there's a div#map to inject into
+    // Only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: "map",
       style: "mapbox://styles/filimonel/ckluqifmv0dvv17o022e90p9k",
     });
 
-    // creates the marker.
+    // Creates the marker.
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
       new mapboxgl.Marker().setLngLat([marker.lng, marker.lat]).addTo(map);
     });
     fitMapToMarkers(map, markers);
+
+    // Zooms in when it goes to location
+    const fitMapToMarkers = (map, markers) => {
+      const bounds = new mapboxgl.LngLatBounds();
+      markers.forEach((marker) => bounds.extend([marker.lng, marker.lat]));
+      map.fitBounds(bounds, { padding: 70, maxZoom: 13, duration: 0 });
+    };
   }
 };
 
 export { initMapbox };
+
+// Map code for view
+
+/* <div id="map"
+style="width: 500px; height: 315px; border-radius: 5px;"
+data-markers="<%= @markers.to_json %>"
+data-mapbox-api-key="<%= ENV['MAPBOX_API_KEY'] %>">
+</div> */
