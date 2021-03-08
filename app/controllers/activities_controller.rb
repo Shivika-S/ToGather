af0f4d1a44d2292498064cdb8e0251cefaacd4af
Sam McCarthy <samrmccarthy6@gmail.com>
2021-03-09 00:38:41 +1100

    Implemented new and create actions in the activities controller. Also added private method to permit params

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -6,8 +6,24 @@ class ActivitiesController < ApplicationController
   end
 
   def new
+    @activity = Activity.new
   end
 
   def create
+    @activity = Activity.new(activity_params)
+    @category = Category.find(params[:category_id])
+    @activity.category = @category
+
+    if @activity.address.present? && @activity.date.present?
+      redirect_to controller: 'activities', action: 'index', id: params[:category_id], address: @activity.address, date: @activity.date
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:date, :address)
   end
 end
