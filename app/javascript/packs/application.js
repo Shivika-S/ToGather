4a8ebb8f7096f0b42978d8a29a0f7b9c477915f4
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-03-08 21:14:46 +1100

    yarn add mapbox

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,7 +29,5 @@ import { initMapbox } from "../plugins/init_mapbox";
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener("turbolinks:load", () => {
-  // Call your functions here, e.g:
-  // initSelect2();
   initMapbox();
 });
