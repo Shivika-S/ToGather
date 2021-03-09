7921d21a1fccf1200797181ca0f4167f538c1ca2
Sam McCarthy <samrmccarthy6@gmail.com>
2021-03-09 11:26:31 +1100

    Added categories and activities to the seeds file and displayed them on the index page by category

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.create(name: "Active")
 Category.create(name: "Romance")
+Category.create(name: "Outdoor")
+
+Category.first.activities.create(name: "Yoga at Inspire 9", address: "1/41-43 Stewart St, Richmond VIC 3121", description: "Do yoga with friends!")
+Category.find(2).activities.create(name: "Cute dinner with Thembi", address: "4/3 Murphy St, South Yarra VIC 3141", description: "Have a cute dinner with Thembi (pop the question at this romantic venue ;)")
+Category.last.activities.create(name: "Walking at the Dandenongs", address: "School Rd, Tremont VIC 3786", description: "Go for a walk through the bush!")
