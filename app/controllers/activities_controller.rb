6eb26eed13aad8419f2da6aadc987952b75f7494
Sam McCarthy <samrmccarthy6@gmail.com>
2021-03-11 14:43:06 +1100

    Added search functionality to filter by date and category

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,10 +1,14 @@
 class ActivitiesController < ApplicationController
   def index
-    activity = Activity.new(activity_params)
-    @activities = Activity.where(category: activity.category)
-    # @activities = Activity.where(category_id: params[:category_id])
-    # @activities = Activity.all
-    @category = activity.category
+    if params[:activity] && activity_params[:category_id] && activity_params[:start_time]
+      @start_time = format_datetime(params[:activity][:start_time])
+      @category = Category.find(params[:activity][:category_id]) if params[:activity][:category_id].present?
+      @activities = Activity.where(category: @category)
+      @activities = activities_on_day(@activities, @start_time)
+    else
+      @activities = Activity.all
+    end
+    @activity = Activity.new
     @random_event = Activity.all.sample
   end
 
@@ -28,6 +32,16 @@ class ActivitiesController < ApplicationController
   end
 
   private
+
+  def format_datetime(date)
+    utc_datetime = Date.parse(date).to_datetime
+    utc_datetime.in_time_zone('Australia/Melbourne') # could add timezone to user and access current_user.timezone here
+    # user_date = utc_date.in_time_zone(time_zone)
+  end
+
+  def activities_on_day(activities, date)
+    activities = activities.where(:start_time => date.beginning_of_day..date.end_of_day)
+  end
 
   def activity_params
     params.require(:activity).permit(:start_time, :address, :category_id)
