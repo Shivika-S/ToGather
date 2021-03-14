bda371be41909685c962d719114818035457912f
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-03-14 17:06:16 +1100

    refactored lines

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,6 @@ class MessagesController < ApplicationController
         @message = Message.new(message_params)
         @message.chatroom = @chatroom
         @message.user = current_user
-
         if @message.save
             ChatroomChannel.broadcast_to(
                 @chatroom,
