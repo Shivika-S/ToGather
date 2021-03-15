d02bb1a2b05b22a86679c36ddbac93d71ab487b2
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-03-16 00:35:18 +1100

    added logic that differentiates between current user messages and external user messages

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -3,5 +3,6 @@ class ChatroomsController < ApplicationController
         @chatroom_index = Chatroom.all
         @chatroom = Chatroom.find(params[:id])
         @message = Message.new
+        @current_user = current_user
     end
 end
