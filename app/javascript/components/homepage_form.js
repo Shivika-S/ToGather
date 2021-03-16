a886607f50dff3c85fc97ec25b8cc1b055159996
Sam McCarthy <samrmccarthy6@gmail.com>
2021-03-16 16:25:41 +1100

    set event listener to go to center of element, not top

--- app/javascript/components/homepage_form.js
+++ app/javascript/components/homepage_form.js
@@ -1,3 +1,15 @@
+const homePageInit = () => {
+  const homePageInitBtn = document.querySelector('.js-home-init-btn');
+  homePageInitBtn.addEventListener('click', function() {
+    const categorySectionEl = document.querySelector('.js-home-category-question');
+    const categoryEl = document.querySelector('.activity_category');
+    window.scrollTo({
+      top: categorySectionEl.offsetTop-((window.innerHeight-categoryEl.offsetHeight)/2),
+      behavior: 'smooth',
+    });
+  });
+}
+
 const chosenCategory = () => {
   const categoryEls = document.querySelectorAll('.form-check-input');
     categoryEls.forEach(categoryEl => {
@@ -10,25 +22,14 @@ const chosenCategory = () => {
           }
         });
         const dateSectionEl = document.querySelector('.js-home-date-question');
+        const dateEl = document.querySelector('#activity_start_time');
         window.scrollTo({
-          top: dateSectionEl.offsetTop,
+          top: dateSectionEl.offsetTop-((window.innerHeight-dateEl.offsetHeight)/2),
           behavior: 'smooth',
         });
       });
     })
   }
-
-const homePageInit = () => {
-  const homePageInitBtn = document.querySelector('.js-home-init-btn');
-  homePageInitBtn.addEventListener('click', function() {
-    const categorySectionEl = document.querySelector('.js-home-category-question');
-    const categoryEl = document.querySelector('.category-buttons');
-    window.scrollTo({
-      top: categorySectionEl.offsetTop-((window.innerHeight-categoryEl.offsetHeight)/2),
-      behavior: 'smooth',
-    });
-  });
-}
 
 export { chosenCategory, homePageInit };
 // window.scrollTo(($(document).width()-$(window).width())/2,0);
