8350db77713a599ef375678814ad0e47dc561449
Sam McCarthy <samrmccarthy6@gmail.com>
2021-03-16 22:45:23 +1100

    Added home page css and javascript to images

--- app/javascript/components/homepage_form.js
+++ app/javascript/components/homepage_form.js
@@ -4,7 +4,7 @@ const homePageInit = () => {
     const categorySectionEl = document.querySelector('.js-home-category-question');
     const categoryEl = document.querySelector('.activity_category');
     window.scrollTo({
-      top: categorySectionEl.offsetTop-((window.innerHeight-categoryEl.offsetHeight)/2),
+      top: categorySectionEl.offsetTop,
       behavior: 'smooth',
     });
   });
@@ -12,19 +12,24 @@ const homePageInit = () => {
 
 const chosenCategory = () => {
   const categoryEls = document.querySelectorAll('.form-check-input');
+  const treeImage = document.querySelector('.tree-image');
     categoryEls.forEach(categoryEl => {
       categoryEl.addEventListener('change', (e) => {
         categoryEls.forEach(categoryEl => {
           if (e.target.checked) {
-            categoryEl.parentNode.style.color = "yellow";
-          } else {
-            categoryEl.parentNode.style.color = "black";
+            setTimeout(() => { treeImage.style.setProperty('bottom', '0px'); }, 1000);
+            setInterval(function(){
+              treeImage.style.setProperty('bottom', '-50px');
+              setTimeout(() => { treeImage.style.setProperty('bottom', '0px'); }, 1000);
+            }, 2000);
+
+
           }
         });
         const dateSectionEl = document.querySelector('.js-home-date-question');
         const dateEl = document.querySelector('#activity_start_time');
         window.scrollTo({
-          top: dateSectionEl.offsetTop-((window.innerHeight-dateEl.offsetHeight)/2),
+          top: dateSectionEl.offsetTop,
           behavior: 'smooth',
         });
       });
