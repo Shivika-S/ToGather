6208049c44ab8ec3965b263fcd2e70610c0bc391
Sam McCarthy <samrmccarthy6@gmail.com>
2021-03-17 21:20:53 +1100

    Made changes to home

--- app/javascript/components/homepage_form.js
+++ app/javascript/components/homepage_form.js
@@ -10,22 +10,8 @@ const homePageInit = () => {
   });
 }
 
-const chosenCategory = () => {
-  const categoryEls = document.querySelectorAll('.form-check-input');
-  const treeImage = document.querySelector('.tree-image');
-    categoryEls.forEach(categoryEl => {
-      categoryEl.addEventListener('click', (e) => {
-        const dateSectionEl = document.querySelector('.js-home-date-question');
-        window.scrollTo({
-          top: dateSectionEl.offsetTop,
-          behavior: 'smooth',
-        });
-      });
-    })
-  }
-
 const outdoorsyCategory = () => {
-  const outdoorsyEl = document.querySelector('.form-check-label[for="activity_category_outdoorsy"]');
+  const outdoorsyEl = document.querySelector('#activity_category_outdoorsy');
   const treeImage = document.querySelector('.tree-image');
       outdoorsyEl.addEventListener('change', (e) => {
           if (e.target.checked) {
@@ -35,8 +21,38 @@ const outdoorsyCategory = () => {
               setTimeout(() => { treeImage.style.setProperty('bottom', '0px'); }, 1000);
             }, 2000);
           };
+          const dateSectionEl = document.querySelector('.js-home-date-question');
+          window.scrollTo({
+            top: dateSectionEl.offsetTop,
+            behavior: 'smooth',
+          });
       });
     }
 
-export { chosenCategory, homePageInit, outdoorsyCategory };
+const chosenCategory = () => {
+  const categoryEls = document.querySelectorAll('.form-check-input');
+    categoryEls.forEach(categoryEl => {
+      categoryEl.addEventListener('click', (e) => {
+        const dateSectionEl = document.querySelector('.js-home-date-question');
+        window.scrollTo({
+          top: dateSectionEl.offsetTop,
+          behavior: 'smooth',
+        });
+        let i = 0;
+        const txt = 'day';
+        const speed = 250;
+        function typeWriter() {
+          if (i < txt.length) {
+            document.getElementById("autotype-day").innerHTML += txt.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+          }
+        }
+        setTimeout(() => { typeWriter(); }, 1500);
+      });
+    })
+  }
+
+
+export { homePageInit, chosenCategory, outdoorsyCategory };
 // window.scrollTo(($(document).width()-$(window).width())/2,0);
