f154a9071ea17f62c38c4b76681b5bfbc389a1d7
Sam McCarthy <samrmccarthy6@gmail.com>
2021-03-18 20:59:01 +1100

    Added background color to save btn

--- app/javascript/components/bookmark_save.js
+++ app/javascript/components/bookmark_save.js
@@ -1,10 +1,29 @@
 const highlightSaveButton = () => {
-  const saveButtons = document.querySelectorAll('.bookmark-buttons');
-  saveButtons.forEach(saveButton => {
+  const indexCards = document.querySelectorAll('.card .bookmark-buttons');
+  indexCards.forEach(indexCard => {
     saveButton.addEventListener('click', (e) => {
-      e.preventDefault();
+      e.stopPropagation();
       console.log('saved');
+
+      if (saveButton.style.background=="white"){
+          saveButton.style.background="purple";
+      }else if(saveButton.style.background=="purple"){
+          saveButton.style.background="white";
+      }
     })
   })
 }
+
 export { highlightSaveButton };
+
+
+
+    document.body.style.background="white";
+    bac.addEventListener("click", function(){
+    if (document.body.style.background=="white"){
+        document.body.style.background="purple";
+    }else if(document.body.style.background=="purple"){
+        document.body.style.background="white";
+    }
+
+    });
