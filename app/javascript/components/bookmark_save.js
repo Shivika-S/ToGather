648520bfc22d096caeccd83f07c028b25fccb0cd
Sam McCarthy <samrmccarthy6@gmail.com>
2021-03-18 21:11:37 +1100

    Added color to the sve btn when saved

--- app/javascript/components/bookmark_save.js
+++ app/javascript/components/bookmark_save.js
@@ -1,29 +1,19 @@
 const highlightSaveButton = () => {
-  const indexCards = document.querySelectorAll('.card .bookmark-buttons');
-  indexCards.forEach(indexCard => {
+  const indexCards = document.querySelectorAll('.card .index-save-btn');
+  indexCards.forEach(saveButton => {
     saveButton.addEventListener('click', (e) => {
       e.stopPropagation();
       console.log('saved');
 
-      if (saveButton.style.background=="white"){
-          saveButton.style.background="purple";
-      }else if(saveButton.style.background=="purple"){
-          saveButton.style.background="white";
+      if (saveButton.classList.contains('btn-outline-secondary')){
+          saveButton.classList.remove('btn-outline-secondary');
+          saveButton.classList.add('btn-secondary');
+      }else if(saveButton.classList.contains('btn-secondary')){
+          saveButton.classList.remove('btn-secondary');
+          saveButton.classList.add('btn-outline-secondary');
       }
     })
   })
 }
 
 export { highlightSaveButton };
-
-
-
-    document.body.style.background="white";
-    bac.addEventListener("click", function(){
-    if (document.body.style.background=="white"){
-        document.body.style.background="purple";
-    }else if(document.body.style.background=="purple"){
-        document.body.style.background="white";
-    }
-
-    });
